646d20a3f23b34601e017e73fea351fd1dcd4b6e
Acks.Eduardo <ackseduardo@gmail.com>
2022-02-27 20:29:07 -0300

    feat: commit inicial

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "api.module.customer-registration"
---
+++ src/test/kotlin/com/aeon/api/module/customerregistration/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aeon.api.module.customerregistration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
